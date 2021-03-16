97b40e7f3c382f5798572f2dbd52abc5b3f5e052
BitManC <yufengzhou1992@163.com>
2021-03-16 17:27:55 +0800

    add transformer

--- RL/Reinforcement_Learning_for_Stock_Prediction/agent/agent.py
+++ RL/Reinforcement_Learning_for_Stock_Prediction/agent/agent.py
@@ -57,8 +57,9 @@ class Agent:
 			if not done:
 				target = reward + self.gamma * np.amax(self.model.predict(next_state)[0])
 
-			target_f = self.model.predict(state)
+			target_f = self.model.predict(state)    #  对下个 action 进行预测，并更改第一个状态
 			target_f[0][action] = target
+
 			self.model.fit(state, target_f, epochs=1, verbose=0)
 
 		if self.epsilon > self.epsilon_min:
